db042059316b19529a682bf11fa9a379e4f2bd07
“ijassm” <ijass.ocean@gmail.com>
2023-09-22 10:40:09 +0530

    list completed -- 22 SEP 2023

--- list.py
+++ list.py
@@ -25,4 +25,77 @@
 
 # print(a)
 
-l = [1, 3, 5, 6, 6]
+# ------------------------------------------
+
+# l = [1, 3, 5, 6, 33, 56, 345]
+
+# length = len(l)
+# acc = 0
+
+# for i in range(length):
+#     acc += l[i]
+
+# for i in l:
+#     acc += i
+
+# print(acc)
+
+# ------------------------------------------
+
+# a = []
+
+# Adds an element at the end of the list
+
+# a.append(22)
+# a.append(45)
+# a.append(6)
+
+# # a.clear()  # Removes all the elements from the list
+
+# copyList = a.copy()  # Returns a copy of the list
+# sameRefList = a
+
+# sameRefList[0] = 111
+
+# # sameRefList.clear()
+
+# print(a)
+# print(sameRefList)
+# print(copyList)
+
+# print(id(a))
+# print(id(copyList))
+# print(id(sameRefList))
+
+# ------------------------------------------
+
+a = [23, 5, 3, 1, 5, 1, 2, 45, 5]
+
+# print(a.count(1))  # Returns the number of elements with the specified value
+
+# someMoreElement = [111, 222, 333, 444]
+
+
+# # Add the elements of a list (or any iterable), to the end of the current list
+# a.extend(someMoreElement)
+# a.append(someMoreElement)
+
+# print(a)
+
+# print(a.index(5))  # Returns the index of the first element with the specified value
+
+# a.insert(1, 12000)  # Adds an element at the specified position
+
+# print(a)
+
+# a.pop(0) #Removes the element at the specified position
+
+# a.remove(5)  # Removes the first item with the specified value
+
+# a.reverse() # Reverses the order of the list
+
+# a.sort(reverse=True)
+
+# print(a)
+
+# ------------------------------------------

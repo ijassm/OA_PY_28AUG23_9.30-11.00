4b8e7117164c5f0c8114503ce97d7f86d9038e13
“ijassm” <ijass.ocean@gmail.com>
2023-09-28 10:42:39 +0530

    set and dict --28 SEP 2023

---
+++ dict.py
@@ -0,0 +1,32 @@
+persons = [
+    {
+        "fname": "ocean",
+        "lname": "academy",
+        "age": 0,
+        "skills": ["HTML", "CSS", "JS", "PY"]
+    },
+    {
+        "fname": "aaa",
+        "lname": "bbb",
+        "age": 0,
+        "skills": ["HTML", "CSS", "JS", "PY"]
+    }
+]
+
+# lessThan18 = []
+
+# Mutable
+
+# person["age"] = 12
+
+# fullname = person["fname"] + " " + person["lname"]
+
+# person["fullname"] = fullname
+
+# print(fullname)
+
+for i in persons:
+    print(i, "\n")
+
+
+# print(persons)
---
+++ set.py
@@ -0,0 +1,6 @@
+s = {1, 2, 4, 5, 67, 2, 4}
+
+# for x in s:
+#     print(x)
+
+# print(2 in s)

8021c9d30ac2a18e3647fbc9fa96b0d037dd420b
“ijassm” <ijass.ocean@gmail.com>
2024-05-09 12:17:24 +0530

    Added : Completed array

---
+++ JS/array.js
@@ -0,0 +1,78 @@
+// console.log("Array");
+
+// let arr = [12, 5, 6, 7];
+
+// // Asscessing indexes
+
+// console.log(arr[0], "arr[0]");
+// console.log(arr[1], "arr[1]");
+// console.log(arr[2], "arr[2]");
+// console.log(arr[3], "arr[3]");
+
+// console.log(arr.at(0), "arr.at(0)");
+// console.log(arr.at(-1), "arr.at(-1)");
+
+// arr.push(15);
+// console.log(arr.length, "arr.length");
+// // console.table(arr, "arr");
+
+// ES6
+// Destructuring
+// rest operator & spread operator
+// syntax =>  ...
+
+// let a = [1, 2, 3];
+// let b = [4, 5, 6];
+// let arr = [...a, ...b];
+
+// arr.push(a);
+// arr.push(b);
+
+// for (let i of a) {
+//   arr.push(i);
+// }
+
+// for (let i of b) {
+//   arr.push(i);
+// }
+
+// console.log(arr);
+
+// spread operator
+
+// arr.push(...a, ...b);
+
+// console.log(arr);
+
+// Destructuring
+
+// const [e1, , , e3, ...rest] = arr;
+
+// console.log(e1);
+// console.log(e3);
+// console.log(rest);
+
+// const a = [1, 2, 4];
+
+// const a = [1, 2, 4];
+// a[0] = 5;
+
+// console.log(a);
+
+// console.log(a);
+
+// for of / for in
+
+// Array.prototype.abc = 54;
+
+// for (let i in a) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i < a.length; i++) {
+//   console.log(a[i]);
+// }
+
+// for (let i of a) {
+//   console.log(i);
+// }
